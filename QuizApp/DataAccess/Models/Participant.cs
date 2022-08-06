d1a33defd77ca2c90fd1f9cccfeac7de7f722457
Amanuel <Amanuel@Melakm.net>
2022-08-06 16:35:44 +0300

    # 1,2,3, 4 Quiz Api project created with reposititory pattern. -Question and participant -Implemented Multiple database support.

---
+++ QuizApp/DataAccess/Models/Participant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Participant
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Score { get; set; }
+        public int TimeTaken { get; set; }
+        public Participant()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
